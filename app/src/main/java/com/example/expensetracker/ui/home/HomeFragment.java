9e7ee58b556bb285ac712fe852b9e050038594f7
Ayoub <ayoubrhiate5@gmail.com>
2025-12-04 20:10:38 +0100

    Add home screen UI

--- app/src/main/java/com/example/expensetracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/expensetracker/ui/home/HomeFragment.java
@@ -1,4 +1,20 @@
 package com.example.expensetracker.ui.home;
 
-public class HomeFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.expensetracker.R; // <-- IMPORTANT : import de R
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // inflate layout fragment_home.xml
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
 }
